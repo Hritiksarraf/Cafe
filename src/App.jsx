e5f1b050ad407d6fd16c72e3df3569cdc141d287
hritiksarraf <hritiksarraf@gmail.com>
2024-01-24 23:24:19 +0530

    fixed name error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/navbar/Navbar'
 import Hero from './components/Hero/Hero'
-import Services from './components/Services/services'
+import Services from './components/Services/Services'
 import Banner from './components/Banner/Banner'
 import AppStore from './components/AppStore/AppStore'
 import Testimonial from './components/Testimonial/Testimonial'
@@ -13,6 +13,7 @@ function App() {
   
 
   return (
+    
     <>
       <Navbar/>
       <Hero/>
